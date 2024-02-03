436d28f8dba1924ecd1ed1406f79e0930233a8c7
umanskiisd <devopsaiproject@gmail.com>
2024-02-03 17:15:21 -0600

    Words to sentence all done

---
+++ Words to sentence - 7kyu.py
@@ -0,0 +1,9 @@
+"""
+Write function which will create a string from a list of strings, separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
+"""
+def words_to_sentence(words):
+    return " ".join(words)
