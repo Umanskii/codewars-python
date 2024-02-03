fcc1060c5ddf2ddd77a21595d73fee40d2a88bca
umanskiisd <devopsaiproject@gmail.com>
2024-02-03 14:20:29 -0600

    7 kyu Find the capitals

---
+++ find_the_capitals.py
@@ -0,0 +1,13 @@
+"""
+Instructions
+Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+Example (Input --> Output)
+"CodEWaRs" --> [0,3,4,6]
+"""
+def capitals(word):
+    indices = []
+    for i in range(len(word)):
+        if word[i] == word[i].upper():
+            indices.append(i)
+    return indices
