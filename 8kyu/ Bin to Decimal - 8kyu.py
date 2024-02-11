8c7863c8320775021ecad2009281c32f33bb7240
umanskiisd <devopsaiproject@gmail.com>
2024-02-11 11:21:06 -0600

    Update format

--- 8kyu/ Bin to Decimal - 8kyu.py
+++ 8kyu/ Bin to Decimal - 8kyu.py
@@ -1,7 +1,33 @@
-""""
+# CodeWars Python Solutions
+
+---
+
+## Bin to Decimal - 8kyu
+
 Complete the function which converts a binary number (given as a string) to a decimal number.
 
 
-"""
+
+### Given Code
+
+
+```python
+def bin_to_decimal(inp):
+    pass
+```
+
+---
+
+### Solution
+
+
+```python
 def bin_to_decimal(inp):
     return int(inp,2)
+```
+
+
+---
+
+
+[See on CodeWars.com](https://https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/)
--- 8kyu/Remove exclamation marks - 8kyu.py
+++
@@ -1,6 +0,0 @@
-"""
-Write function RemoveExclamationMarks which removes all 
-exclamation marks from a given string.
-"""
-def remove_exclamation_marks(s):
-    return s.replace("!", "")
